2d745005ec9de81395f1399f4a0ad214d8d48801
liufeihe <lfhe2014@163.com>
2019-11-21 07:12:18 +0800

    add stores

--- src/counterWithFlux/stores/CounterStore.js
+++ src/counterWithFlux/stores/CounterStore.js
@@ -11,5 +11,28 @@ const counterValues = {
 };
 
 const CounterStore = Object.assign({}, EventEmitter.prototype, {
-  
+  getCounterValues: function(){
+    return counterValues;
+  },
+  emitChange: function(){
+    this.emit(CHANGE_EVENT);
+  },
+  addChangeListener: function(callback){
+    this.on(CHANGE_EVENT, callback);
+  },
+  removeChangeListener: function(callback){
+    this.removeChangeListener(CHANGE_EVENT, callback);
+  }
 });
+
+CounterStore.dispatchToken = AppDispatcher.register((action)=>{
+  if (action.type===ActionTypes.INCREMENT) {
+    counterValues[action.counterCaption]++;
+    CounterStore.emitChange();
+  } else if (action.type===ActionTypes.DECREMENT) {
+    counterValues[action.counterCaption]--;
+    CounterStore.emitChange();
+  } 
+});
+
+export default CounterStore;
